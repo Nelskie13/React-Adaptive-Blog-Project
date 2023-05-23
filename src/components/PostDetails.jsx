0765f13f47ba021e762dd672b52e9b39b0fdeb9e
Jonel Capa <jonelcapa@yahoo.com>
2023-05-23 16:33:03 +0800

    Done designing of PostDetail.jsx

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,5 +1,6 @@
 import commentLogo from "../assets/comment.svg";
-// import heartLogo from "../assets/heart.svg";
+import jenny from "../assets/jenny.svg";
+import unknownPic from "../assets/unknownPic.svg";
 import heartSolid from "../assets/heartSolid.svg";
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
@@ -19,17 +20,20 @@ function PostDetails() {
           <BlogReturn />
           <p className="PostDetails-title">{selectedPost.title}</p>
           <div className="author-date-container">
+            <img src={jenny} />
             <p className="PostDetails-author"> {selectedPost.author}</p>
             <p className="PostDetails-date">{selectedPost.date} </p>
           </div>
           <p className="PostDetails-text">{selectedPost.text}</p>
+
           <div className="likes-author-PostDetails">
             <div className="PostDetails-comment">
               <button className="commentBtn">
                 <img src={commentLogo} alt="comment-logo" />
               </button>
+              <p className="Cards-comment">{selectedPost.comment}k</p>
             </div>
-            <p className="Cards-comment">{selectedPost.comment}k</p>
+
             <div className="PostDetails-heart">
               <button className="heartBtn">
                 <img
@@ -38,17 +42,51 @@ function PostDetails() {
                   alt="heart-logo-solid"
                 />
               </button>
+              <p className="Cards-likes">{selectedPost.likes}k</p>
             </div>
-            <p className="Cards-likes">{selectedPost.likes}k</p>
           </div>
+
           <p className="LeaveComment">Leave a comment:</p>
+
           <div className="Comment-container">
             <input className="textarea" type="text" placeholder="Comments..." />
-            <div className="sendBtn">
-              <p>Send</p>
+            <div className="sendContainer">
+              <p className="sendBtn">Send</p>
             </div>
           </div>
           <p className="line"></p>
+
+          <p className="Comments">Comments: </p>
+        </div>
+        <div className="Comment-List">
+          {selectedPost.comments.map((comment, index) => (
+            <div className="EachComment" key={index}>
+              <div className="unknown-pic">
+                <img src={unknownPic} />
+              </div>
+              <div className="Comment-props">
+                <p className="comment-author">{comment.author}</p>
+                <p className="comment-text">{comment.text}</p>
+                <div className="heart-date-container">
+                  <div className="comment-date">{comment.date}</div>
+                  <div className="Comment-heart">
+                    <button className="heartBtn">
+                      <img
+                        className="comment-heart-solid"
+                        src={heartSolid}
+                        alt="heart-logo-solid"
+                      />
+                    </button>
+                  </div>
+                  <p className="comment-likes">{comment.likes}</p>
+                </div>
+                <p className="Comment-line"></p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="showMore-container">
+          <p className="showMore">Show more</p>
         </div>
       </div>
       <div className="PostDetail-img-container">
